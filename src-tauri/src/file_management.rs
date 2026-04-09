2561ac5f4240924e527d108c6d810b6242769466
Timon Käch <timon@cybertimon.ch>
2026-04-09 22:27:35 +0200

    fix clippy warnings

--- src-tauri/src/file_management.rs
+++ src-tauri/src/file_management.rs
@@ -29,7 +29,6 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use tauri::{AppHandle, Emitter, Manager};
 #[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
-use trash;
 use uuid::Uuid;
 use walkdir::WalkDir;
 
