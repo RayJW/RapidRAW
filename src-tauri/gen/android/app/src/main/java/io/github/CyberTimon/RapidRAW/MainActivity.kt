2bdad442b9a3a4776f070a44443a9bba718f6051
Duecki <linus@kaiserkicker.de>
2026-04-27 15:13:15 +0200

    Use darker grey

--- src-tauri/gen/android/app/src/main/java/io/github/CyberTimon/RapidRAW/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/io/github/CyberTimon/RapidRAW/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : TauriActivity() {
-  private val safeMarginBackgroundColor = Color.rgb(35, 35, 35)
+  private val safeMarginBackgroundColor = Color.rgb(24, 24, 24)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
