f651764af7915fe1e48b43cef79de1d053cf46ba
Duecki <linus@kaiserkicker.de>
2026-04-27 15:00:39 +0200

    Add safe margins

--- src-tauri/gen/android/app/src/main/java/io/github/CyberTimon/RapidRAW/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/io/github/CyberTimon/RapidRAW/MainActivity.kt
@@ -1,11 +1,45 @@
 package io.github.CyberTimon.RapidRAW
 
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+
+    val rootView: View = findViewById(android.R.id.content)
+
+    ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      val ime = insets.getInsets(WindowInsetsCompat.Type.ime())
+      val bottomPadding = if (insets.isVisible(WindowInsetsCompat.Type.ime())) {
+        ime.bottom
+      } else {
+        systemBars.bottom
+      }
+
+      view.setPadding(
+        systemBars.left,
+        systemBars.top,
+        systemBars.right,
+        bottomPadding
+      )
+
+      insets
+    }
+
+    ViewCompat.requestApplyInsets(rootView)
+  }
+
+  override fun onWebViewCreate(webView: WebView) {
+    super.onWebViewCreate(webView)
+
+    webView.setBackgroundColor(0x00000000)
+    webView.fitsSystemWindows = true
   }
 }
